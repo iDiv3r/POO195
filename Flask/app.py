93a79867ee2bb47e73643a598622da12e156cfbc
Div3r <udhgjkf@gmail.com>
2024-05-29 20:22:10 -0600

    Conexion MySQL flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+# "localhost:3308" (puerto de la base de datos)
+
+app.config['MYSQL_USER'] = 'root'
+
+app.config['MYSQL_PASSWORD'] = ''
+
+app.config['MYSQL_DB'] = 'bdFlask'
+
+
+mysql = MySQL(app)
+
+
 
 # Ruta Básica 
 @app.route('/')
@@ -27,10 +42,23 @@ def formulario():
     elif request.method == 'POST':
         return 'POST si es seguro para el envio de pass'
 
-
+# Error Handler 
 @app.errorhandler(404)
 def paginano(e):
     return 'Revisa tu sintaxis, esa url no existe'
+
+
+@app.route('/pruebaConexion')
+def pruebaConexion():
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute("Select 1")
+        datos = cursor.fetchone()
+        return jsonify({'status': 'Conexion exitosa', 'data': datos})
+    
+    except Exception as e:
+        return jsonify({'status': 'Error en la conexion', 'error': str(e)})
+
 
 
 if __name__ == '__main__':
